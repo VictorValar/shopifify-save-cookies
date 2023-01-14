e1214399f635bcd66c58c844b21e111192695921
Valar <valar@victorvalar.me>
2023-01-14 02:37:42 -0300

    add send Cookies to lambda functinality

---
+++ send_cookies_aws.js
@@ -0,0 +1,58 @@
+function sendCookiesToLambdaAPI() {
+    // Get the _fbp, _fbc, gclid, _ttp and ttclid cookies
+    var fbpCookie, fbcCookie, gclidCookie, _ttpCookie, ttclidCookie;
+    try {
+        fbpCookie = getCookie("_fbp");
+        fbcCookie = getCookie("_fbc");
+        gclidCookie = getCookie("gclid");
+        _ttpCookie = getCookie("_ttp");
+        ttclidCookie = getCookie("ttclid");
+    } catch (e) {
+        console.error("Error getting cookies: " + e);
+    }
+
+    // Get the User-Agent request header
+    var userAgent = navigator.userAgent;
+
+    // Get the user's IP address
+    var userIP;
+    fetch("https://api.ipify.org?format=json")
+        .then(response => response.json())
+        .then(data => {
+            userIP = data.ip;
+
+            // Send the data to Lambda HTTP API
+            var payload = {
+                "fbpCookie": fbpCookie,
+                "fbcCookie": fbcCookie,
+                "gclidCookie": gclidCookie,
+                "_ttpCookie": _ttpCookie,
+                "ttclidCookie": ttclidCookie,
+                "userAgent": userAgent,
+                "userIP": userIP
+            }
+            var request = new XMLHttpRequest();
+            request.open("POST", "https://YOUR_LAMBDA_HTTP_API", true);
+            request.setRequestHeader("Content-Type", "application/json");
+            request.onreadystatechange = function() {
+                if (request.readyState === 4) {
+                    if (request.status === 200) {
+                        console.log("Data sent successfully to Lambda HTTP API");
+                    } else {
+                        console.error("Error sending data to Lambda HTTP API. Status: " + request.status);
+                    }
+                }
+            };
+            request.send(JSON.stringify(payload));
+        })
+        .catch(error => {
+            console.error("Error fetching IP address: " + error);
+        });
+}
+
+// Helper function to get a cookie by name
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
