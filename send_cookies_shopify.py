ff53c120f2f905ca55cd1ff7ba25a1322f9a6a9d
Valar <valar@victorvalar.me>
2023-01-19 10:10:54 -0300

    fix cors headers

--- send_cookies_shopify.py
+++ send_cookies_shopify.py
@@ -13,7 +13,6 @@ def options_handler(event, context):
         "statusCode": 200,
         'headers': {
             'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
     }
